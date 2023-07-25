864bd5f6197ba5e571781fbd46cb3431dd6e92fe
AASHIRANSH <AASHIRANSH@GMAIL.COM>
2023-07-25 17:21:31 +0530

    server error fixed

--- learners_academy/views.py
+++ learners_academy/views.py
@@ -5,22 +5,23 @@ from django.contrib.auth.models import User
 import time
 
 def index(request):
-    current_user = request.user
+    user = request.user
     quotes = Quote.objects.all()[0]
     posts = Post.objects.filter(published=True).order_by("-created_at")
     
     now = time.strftime("%a, %d %b %Y %H:%M")
     word = WordOfTheDay.objects.all().order_by("-created_at")[0]
 
-    notifications = []
-    notif = current_user.notification_set.all()
+    if user.is_authenticated:
+        notifications = []
+        notif = user.notification_set.all()
 
-    for n in notif:
-        if n.type == "friend request":
-            notifications.append({
-                "type":"friend request",
-                "sender":n.sender.username
-            })
+        for n in notif:
+            if n.type == "friend request":
+                notifications.append({
+                    "type":"friend request",
+                    "sender":n.sender.username
+                })
             messages.info(request, f"You have a friend request from {n.sender.username}")
 
     vars = {
